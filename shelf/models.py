99cfbb342cd415ee8206b5be5b1f261502b9effb
Armin <armin.derencz@gmail.com>
2018-03-21 18:47:32 +0100

    Utworzenie próbnych modeli

--- shelf/models.py
+++ shelf/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Answer(models.Model):
+    answA = models.CharField(max_length=1)
+    answB = models.CharField(max_length=1)
+    answC = models.CharField(max_length=1)
+    answCorrect = models.CharField(max_length=1)
+
+# Pomiędzy klasami mają być dwie linijki odstępu. 
+class Question(models.Model):
+    title = models.CharField(max_length=100)
+    answer = models.ForeignKey('Answer', on_delete=models.PROTECT) # W nawiasie podajemy nazwę klasy, do której się odwołujemy.
