1703c788605b0918dfcd2e48a95ebd7a09698765
Armin <armin.derencz@gmail.com>
2018-05-20 20:43:18 +0200

    Poprawki w bazie pytań o meteorologii.

--- shelf/admin.py
+++ shelf/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 #Importowanie z modeli
-from .models import Question, Answer # Nigdy nie robić import * w Python!!
+from .models import Question_Meteo, Answer_Meteo # Nigdy nie robić import * w Python!!
 # Register your models here.
 
-admin.site.register([Question, Answer])
+
+class Question_MeteoAdmin(admin.ModelAdmin):#klasa administracyjna dla Question, 
+    search_fields = ['title']
+    list_display = ['title', 'answA', 'answB', 'answC', 'answCorrect']
+    ordering = ['title' ]
+
+
+admin.site.register(Question_Meteo, Question_MeteoAdmin)
+#admin.site.register([Question_Meteo, Answer_Meteo])
--- shelf/models.py
+++ shelf/models.py
@@ -4,19 +4,27 @@ from django.db import models
 
 from django.db import models
 
-class Answer(models.Model):
-    answA = models.CharField(max_length=20)
-    answB = models.CharField(max_length=20)
-    answC = models.CharField(max_length=20)
+class Answer_Meteo(models.Model):
+    id = models.CharField(max_length=2, primary_key=True)
+    name = models.CharField(max_length=100, default='SOME STRING')
+    answA = models.CharField(max_length=100)
+    answB = models.CharField(max_length=100)
+    answC = models.CharField(max_length=100)
     answCorrect = models.CharField(max_length=1)
 
     def __str__(self): #metoda, która wyświetla napis obiektu
-        return "A={answA}; B={answB}; C={answC}; Answer={answCorrect}".format(answA=self.answA, answB=self.answB, answC=self.answC, answCorrect=self.answCorrect)
+        #return "A={answA}; B={answB}; C={answC}; Answer={answCorrect}".format(answA=self.answA, answB=self.answB, answC=self.answC, answCorrect=self.answCorrect)
+        return self.name
+
 
 # Pomiędzy klasami mają być dwie linijki odstępu. 
-class Question(models.Model):
+class Question_Meteo(models.Model):
     title = models.CharField(max_length=100)
-    answer = models.ForeignKey('Answer', on_delete=models.PROTECT) # W nawiasie podajemy nazwę klasy, do której się odwołujemy.
+    #answer = models.ForeignKey('Answer_Meteo', on_delete=models.PROTECT) # W nawiasie podajemy nazwę klasy, do której się odwołujemy.
+    answA = models.CharField(max_length=100)
+    answB = models.CharField(max_length=100)
+    answC = models.CharField(max_length=100)
+    answCorrect = models.CharField(max_length=1)
 
     def __str__(self): #metoda, która wyświetla napis obiektu
         return self.title
