463e8223f8e085d1a61e99104cdf538b7360bc03
Armin <armin.derencz@gmail.com>
2018-05-20 21:38:12 +0200

    Dodanie pustego szablonu - strony html

--- shelf/views.py
+++ shelf/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView #Zawiera widoki generyczne, które zostały przygotowane przez twórców Django do obsługi najpopularniejszych obiektów
 
+from .models import Question_Meteo # Jeśli importujemy coś z submodułu, to odwołujemy się do kropki.
 # Create your views here.
+
+class Question_MeteoListView(ListView):
+    model = Question_Meteo
--- strona_zagle/urls.py
+++ strona_zagle/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+
+from shelf.views import Question_MeteoListView 
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^question_meteo_list/$', Question_MeteoListView.as_view()),
 ]
