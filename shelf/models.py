c1c10162798a9fa50ba3e225b28921616f1e379e
Armin <armin.derencz@gmail.com>
2018-05-20 18:29:25 +0200

    Oznaczenie modeli w panelu administracyjnym - wyswietlanie nazw

--- shelf/models.py
+++ shelf/models.py
@@ -10,7 +10,13 @@ class Answer(models.Model):
     answC = models.CharField(max_length=20)
     answCorrect = models.CharField(max_length=1)
 
+    def __str__(self): #metoda, która wyświetla napis obiektu
+        return "A={answA}; B={answB}; C={answC}; Answer={answCorrect}".format(answA=self.answA, answB=self.answB, answC=self.answC, answCorrect=self.answCorrect)
+
 # Pomiędzy klasami mają być dwie linijki odstępu. 
 class Question(models.Model):
     title = models.CharField(max_length=100)
     answer = models.ForeignKey('Answer', on_delete=models.PROTECT) # W nawiasie podajemy nazwę klasy, do której się odwołujemy.
+
+    def __str__(self): #metoda, która wyświetla napis obiektu
+        return self.title
